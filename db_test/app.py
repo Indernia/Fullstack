650d4588b9e2556cb8d7514a339654d79d4e5506
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2025-03-14 10:21:40 +0100

    added cors allowance

--- db_test/app.py
+++ db_test/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from restaurantAPI import restaurant_api_blueprint
 from flasgger import Swagger
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -8,6 +9,8 @@ app = Flask(__name__)
 app.register_blueprint(restaurant_api_blueprint)
 
 swagger = Swagger(app)
+
+CORS(app, origins="*", methods=["GET", "POST", "PUT", "DELETE"])
 
 if __name__ == '__main__':
     app.run(port=8080)
