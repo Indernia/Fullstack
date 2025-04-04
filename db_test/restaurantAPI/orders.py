a8f2d1805e3d7c512c314dde7dcd36432321cd4f
Alex Lundberg <lundberg.alex1@gmail.com>
2025-04-04 12:47:15 +0200

    added name fix

--- db_test/restaurantAPI/orders.py
+++ db_test/restaurantAPI/orders.py
@@ -69,7 +69,7 @@ def get_orders(restaurantId):
     request_data = query_db("""
                             SELECT
                             o.*,
-                            json_agg(mi)
+                            json_agg(mi) AS menuItems
                             FROM orders o
                             LEFT JOIN orderincludesmenuitem oim ON oim.orderID = o.id
                             LEFT JOIN menuitem mi ON oim.menuItemID = mi.id
