720a56ca179942a2f4e8c0cfefbd7d33ddfa1a08
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2025-04-04 11:10:17 +0200

    fix

--- db_test/restaurantAPI/restaurants.py
+++ db_test/restaurantAPI/restaurants.py
@@ -147,7 +147,7 @@ def add_restaurant():
     latitude = data.get("latitude")
     longitude = data.get("longitude")
 
-    insert_db('INSERT INTO restaurant (name, chainID, latitude, longitude, ownerID) VALUES (%s, %s, %s, %s)',
+    insert_db('INSERT INTO restaurant (name, chainID, latitude, longitude, ownerID) VALUES (%s, %s, %s, %s, %s)',
               args=(name, chainID, latitude, longitude, ownerID))
     return jsonify({"message": "Restaurant added successfully"}), 200
 
