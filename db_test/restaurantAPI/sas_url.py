75f15b172d26f77e31abd62c9a90884c26cdf73b
NN0323 <noah@dimse.dk>
2025-04-21 10:32:24 +0200

    didnt save properly

--- db_test/restaurantAPI/sas_url.py
+++ db_test/restaurantAPI/sas_url.py
@@ -8,20 +8,26 @@ import os
 account_name = os.getenv("AZURE_STORAGE_ACCOUNT")
 account_key = os.getenv("AZURE_STORAGE_ACCESS_KEY")
 container_name = "menu-items"  # Name of your Azure Blob Storage container
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 # Generate SAS URL for image upload
-@app.route('api/SASURL', methods=['POST'])
+@app.route('/SASURL', methods=['POST'])
 @jwt_required()
 def generate_sas_url():
     file_name = request.json.get("fileName")
 
     # Initialize the BlobServiceClient
-    blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
+    blob_service_client = BlobServiceClient(
+        account_url=f"https://{account_name}.blob.core.windows.net", 
+        credential=account_key
+    )
     
     # Get the Blob client for the specific file
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name, 
+        blob=file_name
+    )
     
     # Set SAS expiration time (e.g., 15 minutes)
     sas_expiration = datetime.now() + timedelta(minutes=15)
@@ -31,7 +37,7 @@ def generate_sas_url():
         account_name=account_name,
         container_name=container_name,
         blob_name=file_name,
-        permission=BlobSASPermissions(write=True),
+        permission=BlobSASPermissions(write=True, create=True),
         expiry=sas_expiration
     )
     
