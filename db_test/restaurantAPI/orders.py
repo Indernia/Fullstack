1726281af4ec8f6897b36534b293c84655c41736
Alex Lundberg <lundberg.alex1@gmail.com>
2025-04-23 22:07:14 +0200

    added order id to the add order endpoint

--- db_test/restaurantAPI/orders.py
+++ db_test/restaurantAPI/orders.py
@@ -114,54 +114,54 @@ def get_order(orderId):
 
 @orders_blueprint.route('/orders/add', methods=["POST"])
 @swag_from({
-    'tags': ['Orders'],
-    'parameters': [{
-        'in': 'body',
-        'name': 'body',
-        'required': True,
-        'schema': {
-            'type': 'object',
-            'properties': {
-                'restaurantId': {
+'tags': ['Orders'],
+'parameters': [{
+    'in': 'body',
+    'name': 'body',
+    'required': True,
+    'schema': {
+        'type': 'object',
+        'properties': {
+            'restaurantId': {
+                'type': 'integer',
+                'description': 'The ID of the restaurant this order is for'
+            },
+            'userId': {
+                'type': 'integer',
+                'description': 'The ID of the customer who placed the order'
+            },
+            'orderTable': {
+                'type': 'integer',
+                'description': 'The table number the order is for'
+            },
+            'orderTime': {
+                'type': 'string',
+                'description': 'The time the order was placed'
+            },
+            'comments': {
+                'type': 'string',
+                'description': 'any comments there may be for the order, can be blank'
+            },
+            'menuItems': {
+                'type': 'array',
+                'items': {
                     'type': 'integer',
-                    'description': 'The ID of the restaurant this order is for'
-                },
-                'userId': {
-                    'type': 'integer',
-                    'description': 'The ID of the customer who placed the order'
-                },
-                'orderTable': {
-                    'type': 'integer',
-                    'description': 'The table number the order is for'
-                },
-                'orderTime': {
-                    'type': 'string',
-                    'description': 'The time the order was placed'
-                },
-                'comments': {
-                    'type': 'string',
-                    'description': 'any comments there may be for the order, can be blank'
-                },
-                'menuItems': {
-                    'type': 'array',
-                    'items': {
-                        'type': 'integer',
-                        'description': 'The ID of a menu item'
-                    }
+                    'description': 'The ID of a menu item'
                 }
             }
         }
-    }],
-    'responses': {
-        201: {
-            'description': 'Order created'
-        },
-        400: {
-            'description': 'Bad request'
-        }},
-    })
+    }
+}],
+'responses': {
+    201: {
+        'description': 'Order created'
+    },
+    400: {
+        'description': 'Bad request'
+    }},
+})
 def add_order():
-    # TODO: Add better rollback for none existing menu items
+# TODO: Add better rollback for none existing menu items
     request_data = request.get_json()
     restaurantId = request_data['restaurantId']
     userId = request_data['userId']
@@ -185,27 +185,27 @@ def add_order():
         insert_db("INSERT INTO orderincludesmenuitem (orderID, menuItemID) VALUES (%s, %s)",
                   args=(orderID, item))
 
-    return jsonify({"message": "Order created"}), 201
+    return jsonify({"message": f"Order created: {orderID}"}), 201
 
 
 @orders_blueprint.route('/orders/markComplete/<orderID>/', methods=["PUT"])
 @swag_from({
-    'tags': ['Orders'],
-    'parameters': [{
-        'name': 'orderId',
-        'in': 'path',
-        'description': 'The ID of the order to mark as complete',
-        'required': True,
-        'type': 'integer'
-    }],
-    'responses': {
-        200: {
-            'description': 'Order marked as complete'
-        },
-        404: {
-            'description': 'Order not found'
-        }},
-    })
+'tags': ['Orders'],
+'parameters': [{
+    'name': 'orderId',
+    'in': 'path',
+    'description': 'The ID of the order to mark as complete',
+    'required': True,
+    'type': 'integer'
+}],
+'responses': {
+    200: {
+        'description': 'Order marked as complete'
+    },
+    404: {
+        'description': 'Order not found'
+    }},
+})
 def mark_order_complete(orderID):
     insert_db("UPDATE orders SET orderComplete = TRUE WHERE id = %s", args=(orderID,))
     return jsonify({"message": "Order marked as complete"}), 200
@@ -214,31 +214,31 @@ def mark_order_complete(orderID):
 
 @orders_blueprint.route('/orders/items/<orderId>/', methods=["GET"])
 @swag_from({
-    'tags': ['Orders'],
-    'parameters': [{
-        'name': 'orderId',
-        'in': 'path',
-        'description': 'The ID of the order to get items from',
-        'required': True,
-        'type': 'integer'
-    }],
-    'responses': {
-        200: {
-            'description': 'Items in the order',
-            'schema': {
-                'type': 'object',
-                'properties': {
-                    'menuItemID': {
-                        'type': 'integer',
-                        'description': 'The ID of the menu item'
-                    }
+'tags': ['Orders'],
+'parameters': [{
+    'name': 'orderId',
+    'in': 'path',
+    'description': 'The ID of the order to get items from',
+    'required': True,
+    'type': 'integer'
+}],
+'responses': {
+    200: {
+        'description': 'Items in the order',
+        'schema': {
+            'type': 'object',
+            'properties': {
+                'menuItemID': {
+                    'type': 'integer',
+                    'description': 'The ID of the menu item'
                 }
             }
-        },
-        404: {
-            'description': 'Order not found'
-        }},
-    })
+        }
+    },
+    404: {
+        'description': 'Order not found'
+    }},
+})
 def get_order_items(orderId):
     request_data = query_db("""
                             SELECT mi.*
